21a013213488d0b75321b51bba502652c1a5679c
cathy <cathy@bubulazi.com>
2022-12-23 13:03:51 +0000

    Terminada la parte estática

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
 import Cuadrado from './componentes/Cuadrado'
+import data from './assets/data.js'
 
 
 function App() {
-  
+  const html = data.map( cuadrado => {
+   return <Cuadrado 
+      key={cuadrado.id}
+      id={cuadrado.id}
+      on={cuadrado.on}
+    />
+  })
 
   return (
-    <div className="App">
-      <Cuadrado />
+    <div>
+      {html}
     </div>
   )
 }
 
 export default App
+
+// export default [
+//   {
+//       id: 1,
+//       on: true
+//   },   
+//   {
+//       id: 2,
+//       on: false
+//   },   
+//   {
+//       id: 3,
+//       on: true
+//   },   
+//   {
+//       id: 4,
+//       on: true
+//   },   
+//   {
+//       id: 5,
+//       on: false
+//   },   
+//   {
+//       id: 6,
+//       on: false
+//   },   
+// ]
--- src/componentes/Cuadrado.jsx
+++ src/componentes/Cuadrado.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 
-export default function Cuadrado() {
+export default function Cuadrado(props) {
+
+  const color = props.on ? 'orange' : 'transparent'
+
+
   return (
-    <div className='cuadrado'>
-     
+    <div style={{ backgroundColor: color }} className='cuadrado'>
+        <p>{props.id}</p>
 
     </div>
   )
 }
+
+// props {
+//   id: 1,
+//   on: true
+// }
