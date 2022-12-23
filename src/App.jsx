8bc6db69cc5a4ee4f9e5b3d3af5b588089fda921
cathy <cathy@bubulazi.com>
2022-12-23 12:08:32 +0000

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cuadrado from './componentes/Cuadrado'
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Cuadrado />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Cuadrado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Cuadrado() {
+  return (
+    <div className='cuadrado'>
+     
+
+    </div>
+  )
+}
